50410ae8857d27eedc5d6345ca84960086654706
Maria Dimitrova <maria.dimitrova@helsinki.fi>
2019-11-07 22:20:12 +0200

    Fixed dates of the talks

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,8 @@
 var addressPoints = [
   [
-    "Finland",
-    63.2467777,
-    25.9209164
+    "Salerno, Italy",
+    40.6803601,
+    14.7594542
   ],
   [
     "Helsinki, Finland",
@@ -10,8 +10,8 @@ var addressPoints = [
     24.9425683
   ],
   [
-    "Salerno, Italy",
-    40.6803601,
-    14.7594542
+    "Kuopio, Finland",
+    62.8241424,
+    27.594561484489
   ]
 ];
